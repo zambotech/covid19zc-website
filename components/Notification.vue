401b719c07e4fbd3384033353fedaffec96720d6
JHNLWHD <aljhoenilw@gmail.com>
2020-03-28 11:48:06 +0800

    feat: add notification component for legitimacy of the data purposes.

---
+++ components/Notification.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="flex bg-blue-100 p-6">
+    <div class="flex items-center text-blue-500 mr-6">
+      <svg
+        class="fill-current h-8 w-8 mr-2"
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M22 18H18V30H22V18Z" fill="#4299E1" />
+        <path
+          d="M20 0C8.95002 0 0 8.95002 0 20C0 31.0499 8.95002 39.9999 20 39.9999C31.0499 39.9999 40 31.05 40 20C40 8.94993 31.05 0 20 0ZM20 36C11.18 36 3.99993 28.8199 3.99993 20C3.99993 11.18 11.18 4.00003 20 4.00003C28.8199 4.00003 36 11.18 36 20C36 28.8199 28.8199 36 20 36Z"
+          fill="#4299E1"
+        />
+        <path d="M22 10H18V14.0001H22V10Z" fill="#4299E1" />
+      </svg>
+      <span
+        class="font-semibold xl:text-xl lg:text-lg md:text-lg sm:text-sm tracking-tight"
+      >
+        Data sources are coming from
+        <a
+          class="underline"
+          href="https://www.facebook.com/bengclimaco/"
+          target="_blank"
+          >Beng Climaco</a
+        >
+        and
+        <a
+          class="underline"
+          href="https://www.facebook.com/dohzpchd/"
+          target="_blank"
+          >DOH RO IX</a
+        >
+        Facebook Page
+      </span>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,40 @@
 <template>
-  <div class="max-w-4xl m-auto">
-    <section class="flex justify-between pt-8">
-      <CardCounter
-        legend-color="orange"
-        card-title="Persons Under Investigation"
-        :total-cases="33"
-        :increase="7"
-        :decrease="11"
-      />
-      <CardCounter
-        legend-color="yellow"
-        card-title="Persons Under Monitoring"
-        :total-cases="712"
-        :increase="52"
-        :decrease="184"
-      />
-      <CardCounter
-        legend-color="red"
-        card-title="Case Confirmed"
-        :total-cases="1"
-        :increase="1"
-      />
-    </section>
+  <div>
+    <Notification />
+    <div class="max-w-4xl m-auto">
+      <section class="flex justify-between pt-8">
+        <CardCounter
+          legend-color="orange"
+          card-title="Persons Under Investigation"
+          :total-cases="33"
+          :increase="7"
+          :decrease="11"
+        />
+        <CardCounter
+          legend-color="yellow"
+          card-title="Persons Under Monitoring"
+          :total-cases="712"
+          :increase="52"
+          :decrease="184"
+        />
+        <CardCounter
+          legend-color="red"
+          card-title="Case Confirmed"
+          :total-cases="1"
+          :increase="1"
+        />
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import Notification from '@/components/Notification.vue'
 import CardCounter from '@/components/CardCounter.vue'
 
 export default {
   components: {
+    Notification,
     CardCounter
   }
 }
