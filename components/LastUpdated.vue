41cae0f2a6bab22f0d8a5d1b736131e284135f47
K. Usman <kusman758@gmail.com>
2020-03-28 13:19:28 +0800

    feat: Added Last Updated Component

---
+++ components/LastUpdated.vue
@@ -0,0 +1,15 @@
+<template>
+  <label class="block text-gray-500 text-sm">
+    Last Updated: {{ lastUpdated }}
+  </label>
+</template>
+<script>
+export default {
+  props: {
+    lastUpdated: {
+      type: Date,
+      default: ''
+    }
+  }
+}
+</script>
