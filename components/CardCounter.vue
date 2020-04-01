a4ae97b328c7033c635f28c019f0f6fa4da5d0c2
jbienesdev <jbienes.dev@gmail.com>
2020-04-01 20:02:53 +0800

    fix: add meta tags, convert to svg, change favicon

--- components/CardCounter.vue
+++ components/CardCounter.vue
@@ -3,11 +3,16 @@
     class="p-8 shadow-md inline-block rounded-md w-full lg:w-auto bg-white lg:mb-0 mb-8"
   >
     <div class="flex items-center">
-      <div
-        :class="`bg-${legendColor}-400`"
-        class="rounded-full w-2 h-2 inline-block mr-2"
-      ></div>
-      <h3 class="text-gray-600 text-sm inline-block">
+      <svg
+        width="10"
+        height="10"
+        viewBox="0 0 10 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="5" cy="5" r="5" :fill="legendColor" />
+      </svg>
+      <h3 class="text-gray-600 text-sm inline-block ml-2">
         {{ cardTitle }}
       </h3>
     </div>
