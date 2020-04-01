7c66eb29de38ae28608a84aa5e62ad1d8c5d324e
johnmarkovalo <johnmarkovalo@gmail.com>
2020-04-01 16:04:27 +0800

    refactor: add updated date for each cards

--- pages/index.vue
+++ pages/index.vue
@@ -3,29 +3,26 @@
     <Notification />
     <div class="max-w-4xl m-auto px-8 lg:px-0">
       <Header title="COVID-19 Tracker: Zamboanga City"></Header>
-      <LastUpdated last-updated="Wed, Mar 25, 2020 9:00 PM"></LastUpdated>
       <section
         class="flex justify-between mt-8 mb-8 lg:mb-12 flex-wrap lg:px-0"
       >
         <CardCounter
           legend-color="orange"
           card-title="Persons Under Investigation"
+          last-updated="March 25, 9:00 PM"
           :total-cases="33"
-          :increase="7"
-          :decrease="11"
         />
         <CardCounter
           legend-color="yellow"
           card-title="Persons Under Monitoring"
+          last-updated="March 25, 9:00 PM"
           :total-cases="712"
-          :increase="52"
-          :decrease="184"
         />
         <CardCounter
           legend-color="red"
           card-title="Case Confirmed"
+          last-updated="March 25, 9:00 PM"
           :total-cases="1"
-          :increase="1"
         />
       </section>
       <div class="border-b-2 w-64 m-auto border-gray-200 mt-4"></div>
@@ -194,7 +191,6 @@
 import Notification from '@/components/Notification.vue'
 import CardCounter from '@/components/CardCounter.vue'
 import Header from '@/components/Header.vue'
-import LastUpdated from '@/components/LastUpdated.vue'
 import Accordion from '@/components/Accordion.vue'
 
 export default {
@@ -202,7 +198,6 @@ export default {
     Notification,
     CardCounter,
     Header,
-    LastUpdated,
     Accordion
   }
 }
