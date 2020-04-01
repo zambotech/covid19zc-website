ddb4b58571fcbb5db3fed05727300ede4f34d826
jbienesdev <jbienes.dev@gmail.com>
2020-04-01 21:17:11 +0800

    feat: add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,13 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: process.env.GA_ID
+      }
+    ]
   ],
   /*
    ** Nuxt.js modules
