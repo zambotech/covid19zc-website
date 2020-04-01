fc2ecf1991924218d17326860ca01defed8b34fb
JHNLWHD <aljhoenilw@gmail.com>
2020-04-01 09:25:24 +0800

    :chore changed website title to COVID-19 Tracker: Zamboanga City and website description to Tracking...

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'COVID-19 Tracker: Zamboanga City',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
