c1ddf6802986b73a59ed9dfe43ab8e2abd5e05ca
johnmarkovalo <johnmarkovalo@gmail.com>
2020-04-01 16:04:27 +0800

    refactor: add updated date for each cards

--- components/CardCounter.vue
+++ components/CardCounter.vue
@@ -11,42 +11,11 @@
         {{ cardTitle }}
       </h3>
     </div>
-    <div class="flex items-center">
+    <div class="flex justify-center">
       <h4 class="text-gray-800 font-bold text-6xl">{{ totalCases }}</h4>
-      <div class="ml-8">
-        <div
-          class="flex items-center text-red-500 bg-red-100 px-4 py-1 text-sm font-bold rounded-full mb-2"
-        >
-          <svg
-            width="10"
-            height="10"
-            viewBox="0 0 10 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M5 0L10 10H0L5 0Z" fill="#F56565" />
-          </svg>
-          <p class="ml-2">{{ increase }}</p>
-        </div>
-        <div
-          v-if="decrease"
-          class="flex items-center text-green-500 bg-green-100 px-4 py-1 text-sm font-bold rounded-full"
-        >
-          <svg
-            width="10"
-            height="11"
-            viewBox="0 0 10 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M5 10.3301L1.11265e-06 0.33013L10 0.330131L5 10.3301Z"
-              fill="#68D391"
-            />
-          </svg>
-          <p class="ml-2">{{ decrease }}</p>
-        </div>
-      </div>
+    </div>
+    <div class="flex justify-center">
+      <h4 class="text-gray-600 font-light text-xs">As of {{ lastUpdated }}</h4>
     </div>
   </div>
 </template>
@@ -66,13 +35,9 @@ export default {
       type: Number,
       default: 0
     },
-    increase: {
-      type: [Number, null],
-      default: null
-    },
-    decrease: {
-      type: [Number, null],
-      default: null
+    lastUpdated: {
+      type: String,
+      default: ''
     }
   }
 }
