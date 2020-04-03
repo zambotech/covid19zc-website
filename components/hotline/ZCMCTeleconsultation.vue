5f47a068889e6aa1e2a2f6e46282f676a88493f1
JHNLWHD <aljhoenilw@gmail.com>
2020-04-03 20:42:30 +0800

    refactor: separate hotline numbers into two separate components. fix: update zamboanga task force covid-19 hotline numbers.

---
+++ components/hotline/ZCMCTeleconsultation.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="flex flex-wrap py-6 justify-center text-center">
+    <div class="my-4 w-full lg:w-1/2">
+      <div class="font-semibold text-gray-700 mb-6">
+        <p>8:00AM to 12:00NN (Monday to Saturday)</p>
+      </div>
+      <div class="mb-6">
+        <p class="italic text-gray-700">Smart / Talk N’ Text / Sun</p>
+        <div class="flex justify-center text-gray-700 font-light mt-2 text-sm">
+          <div class="mr-6">
+            <p>0938-3004-011</p>
+            <p>0938-3004-012</p>
+            <p>0938-3004-013</p>
+            <p>0938-3004-014</p>
+            <p>0938-3004-015</p>
+            <p>0938-3004-016</p>
+          </div>
+          <div>
+            <p>0938-3004-017</p>
+            <p>0938-3004-018</p>
+            <p>0938-3004-019</p>
+            <p>0938-3004-020</p>
+            <p>0938-3004-021</p>
+            <p>0938-3004-022</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p class="italic text-gray-700">Globe / TM</p>
+        <div class="flex justify-center text-gray-700 font-light mt-2 text-sm">
+          <div class="mr-6">
+            <p>0906-6538-602</p>
+            <p>0953-1489-518</p>
+            <p>0926-9684-935</p>
+            <p>0955-3309-551</p>
+            <p>0955-9103-830</p>
+            <p>0975-0737-270</p>
+          </div>
+          <div>
+            <p>0955-9103-876</p>
+            <p>0955-3309-569</p>
+            <p>0955-3309-582</p>
+            <p>0975-0737-267</p>
+            <p>0975-0730-159</p>
+            <p>0926-9684-980</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="my-4 w-full lg:w-1/2">
+      <div class="font-semibold text-gray-700">
+        <p>12:00NN to 4:00PM (Monday to Friday)</p>
+        <p>8:00AM to 12:00NN (Sunday)</p>
+      </div>
+      <div class="mb-6">
+        <p class="italic text-gray-700">Smart / Talk N’ Text / Sun</p>
+        <div class="flex justify-center text-gray-700 font-light mt-2 text-sm">
+          <div class="mr-6">
+            <p>0938-3004-023</p>
+            <p>0938-3004-024</p>
+            <p>0938-3004-025</p>
+            <p>0938-3004-026</p>
+            <p>0938-3004-027</p>
+            <p>0938-3004-028</p>
+          </div>
+          <div>
+            <p>0938-3004-029</p>
+            <p>0938-3004-030</p>
+            <p>0938-3004-031</p>
+            <p>0938-3004-032</p>
+            <p>0938-3004-033</p>
+            <p>0938-3004-034</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p class="italic text-gray-700">Globe / TM</p>
+        <div class="flex justify-center text-gray-700 font-light mt-2 text-sm">
+          <div class="mr-6">
+            <p>0975-0737-268</p>
+            <p>0906-6538-608</p>
+            <p>0955-9103-850</p>
+            <p>0955-9103-911</p>
+            <p>0955-9103-863</p>
+            <p>0975-0730-147</p>
+            <p>0975-0730-148</p>
+          </div>
+          <div>
+            <p>0975-0730-149</p>
+            <p>0975-0730-150</p>
+            <p>0975-0730-151</p>
+            <p>0975-0730-152</p>
+            <p>0975-0730-153</p>
+            <p>0975-0730-154</p>
+            <p>0975-0730-155</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/hotline/ZTFCOVID19.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex flex-wrap py-6 justify-center">
+    <div class="my-4 lg:my-0 mx-0 lg:mx-4 text-center w-1/2 lg:w-auto">
+      <h1 class="font-semibold text-gray-700">Globe Hotlines</h1>
+      <div class="text-gray-700 font-light mt-2 text-sm">
+        <p>0975-073-0339</p>
+        <p>0975-073-0338</p>
+        <p>0975-073-0337</p>
+        <p>0975-073-0336</p>
+        <p>0975-073-0335</p>
+      </div>
+    </div>
+    <div class="my-4 lg:my-0 mx-0 lg:mx-4 text-center w-1/2 lg:w-auto">
+      <h1 class="font-semibold text-gray-700">Smart Hotlines</h1>
+      <div class="text-gray-700 font-light mt-2 text-sm">
+        <p>0998-175-8705</p>
+        <p>0998-175-8709</p>
+        <p>0938-300-4110</p>
+        <p>0938-300-4007</p>
+        <p>0938-300-4004</p>
+      </div>
+    </div>
+    <div class="my-4 lg:my-0 mx-0 lg:mx-4 text-center w-1/2 lg:w-auto">
+      <h1 class="font-semibold text-gray-700">Health Lines</h1>
+      <div class="text-gray-700 font-light mt-2 text-sm">
+        <p>0975-073-0150</p>
+        <p>0975-073-0151</p>
+        <p>0966-175-4298</p>
+        <p>0909-371-3046</p>
+      </div>
+    </div>
+    <div class="my-4 lg:my-0 mx-0 lg:mx-4 text-center w-1/2 lg:w-auto">
+      <h1 class="font-semibold text-gray-700">Police Operations</h1>
+      <div class="text-gray-700 font-light mt-2 text-sm">
+        <p>0977-855-8138</p>
+        <p>0917-165-6672</p>
+        <p>0936-590-6780</p>
+      </div>
+    </div>
+    <div class="my-4 lg:my-0 mx-0 lg:mx-4 text-center w-1/2 lg:w-auto">
+      <h1 class="font-semibold text-gray-700">ZCDRRMO</h1>
+      <div class="text-gray-700 font-light mt-2 text-sm">
+        <p>955-9601</p>
+      </div>
+    </div>
+  </div>
+</template>
