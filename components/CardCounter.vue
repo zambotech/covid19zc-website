ec77534385c01beb79705c516f01603ff31d8f35
jbienesdev <jbienes.dev@gmail.com>
2020-04-04 14:42:06 +0800

    fix: redesign of the website

--- components/CardCounter.vue
+++ components/CardCounter.vue
@@ -1,27 +1,8 @@
 <template>
-  <div
-    class="p-8 shadow-md inline-block rounded-md w-full lg:w-auto bg-white lg:mb-0 mb-8"
-  >
-    <div class="flex items-center">
-      <svg
-        width="10"
-        height="10"
-        viewBox="0 0 10 10"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle cx="5" cy="5" r="5" :fill="legendColor" />
-      </svg>
-      <h3 class="text-gray-600 text-sm inline-block ml-2">
-        {{ cardTitle }}
-      </h3>
-    </div>
-    <div class="flex justify-center">
-      <h4 class="text-gray-800 font-bold text-6xl">{{ totalCases }}</h4>
-    </div>
-    <div class="flex justify-center">
-      <h4 class="text-gray-600 font-light text-xs">As of {{ lastUpdated }}</h4>
-    </div>
+  <div class="flex items-center flex-col py-6 rounded-lg bg-white lg:mb-0 mb-8">
+    <h3 class="text-primary font-semibold text-sm lg:text-base">{{ cardTitle }}</h3>
+    <h4 class="text-purple-900 font-bold text-5xl lg:text-6xl">{{ totalCases }}</h4>
+    <h4 class="text-primary font-light text-xs">As of {{ lastUpdated }}</h4>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,27 @@
 <template>
   <div>
-    <Notification />
-    <div class="max-w-4xl m-auto px-8 lg:px-0">
-      <Header title="COVID-19 Tracker: Zamboanga City"></Header>
-      <section
-        class="flex justify-between mt-8 mb-8 lg:mb-12 flex-wrap lg:px-0"
-      >
+    <div>
+      <section class="flex justify-between flex-wrap lg:w-full xl:w-4/6 my-8 lg:mb-12">
         <CardCounter
-          legend-color="#F6AD55"
+          class="md:mr-6 w-full md:w-auto flex-none md:flex-1"
           card-title="Persons Under Investigation"
           last-updated="April 2, 2020"
           :total-cases="55"
         />
         <CardCounter
-          legend-color="#F6E05E"
+          class="md:mr-6 w-full md:w-auto flex-none md:flex-1"
           card-title="Persons Under Monitoring"
           last-updated="April 1, 2020 10:27 PM"
           :total-cases="2381"
         />
         <CardCounter
-          legend-color="#F56565"
+          class="w-full md:w-auto flex-none md:flex-1"
           card-title="Cases Confirmed"
           last-updated="April 4, 2020 10:09 AM"
           :total-cases="4"
         />
       </section>
-      <div class="border-b-2 w-64 m-auto border-gray-200 mt-4"></div>
-      <Header class="mb-2" title="Hotline Numbers"></Header>
+      <p class="text-purple-900 font-bold text-3xl">Hotline Numbers</p>
       <section class="grid gap-4 mb-24">
         <Accordion title="Zamboanga Task Force COVID-19" :visible="true">
           <ZTFCOVID19 />
@@ -43,18 +38,14 @@
 </template>
 
 <script>
-import Notification from '@/components/Notification.vue'
 import CardCounter from '@/components/CardCounter.vue'
-import Header from '@/components/Header.vue'
 import Accordion from '@/components/Accordion.vue'
 import ZTFCOVID19 from '@/components/hotline/ZTFCOVID19.vue'
 import ZCMCTeleconsultation from '@/components/hotline/ZCMCTeleconsultation.vue'
 
 export default {
   components: {
-    Notification,
     CardCounter,
-    Header,
     Accordion,
     ZTFCOVID19,
     ZCMCTeleconsultation
