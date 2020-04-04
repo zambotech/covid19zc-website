17512946a80ba7226874c5e3e001172683eb7f00
jbienesdev <jbienes.dev@gmail.com>
2020-04-04 19:04:26 +0800

    feat: add maintenance page

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="maintenance-container flex flex-col justify-center items-center text-center">
+    <img class="maintenance-image" src="~/assets/images/maintenance.svg">
+    <h1 class="font-bold text-purple-900 text-2xl md:text-3xl md">This page is under maintenance.</h1>
+    <h2 class="text-primary text-sm md:text-lg">Just come back later for more updates.</h2>
+    <nuxt-link to="/" class="underline text-primary text-sm md:text-lg">Go to home</nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+.maintenance-container {
+  height: 70vh;
+}
+
+.maintenance-image {
+  width: 40%;
+  height: auto;
+}
+
+@media (max-width: 768px) {
+  .maintenance-image {
+    width: 100%;
+    height: auto;
+  } 
+}
+</style>
