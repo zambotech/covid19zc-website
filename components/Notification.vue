50e79f7f069b8b3c57466bf9ea102875a4d4a8b1
JHNLWHD <aljhoenilw@gmail.com>
2020-04-12 17:59:24 +0800

    doc: update data sources as CHO already collaborated with the team.

--- components/Notification.vue
+++ components/Notification.vue
@@ -1,13 +1,7 @@
 <template>
   <section class="flex justify-center bg-primary p-4">
     <div class="flex items-center">
-      <svg
-        width="25"
-        height="25"
-        viewBox="0 0 25 25"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M13.75 11.25H11.25V18.75H13.75V11.25Z" fill="white" />
         <path
           d="M12.5 0C5.59376 0 0 5.59376 0 12.5C0 19.4062 5.59376 24.9999 12.5 24.9999C19.4062 24.9999 25 19.4062 25 12.5C25 5.59371 19.4062 0 12.5 0ZM12.5 22.5C6.98747 22.5 2.49996 18.0125 2.49996 12.5C2.49996 6.98747 6.98747 2.50002 12.5 2.50002C18.0125 2.50002 22.5 6.98747 22.5 12.5C22.5 18.0125 18.0125 22.5 12.5 22.5Z"
@@ -15,23 +9,26 @@
         />
         <path d="M13.75 6.25003H11.25V8.75005H13.75V6.25003Z" fill="white" />
       </svg>
-
       <span
-        class="font-semibold text-white ml-2 text-sm lg:text-lg tracking-tight"
-      >
-        Data sources are coming from
+        class="font-semibold text-white ml-2 text-sm lg:text-lg tracking-tight">
+        Data sources are coming from City Health Office,
         <a
           class="underline"
           href="https://www.facebook.com/bengclimaco/"
           target="_blank"
         >Beng Climaco</a>
-        and
+        ,
         <a
           class="underline"
           href="https://www.facebook.com/dohzpchd/"
           target="_blank"
         >DOH RO IX</a>
-        Facebook Page
+        and
+        <a
+          class="underline"
+          href="https://www.facebook.com/piawesternmindanao/"
+          target="_blank"
+        >PIA Western Mindanao</a>
       </span>
     </div>
   </section>
